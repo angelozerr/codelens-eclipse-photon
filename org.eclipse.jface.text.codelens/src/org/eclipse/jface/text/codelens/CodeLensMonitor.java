ff0687a35ab577b73ec362b93649e895a0488a41
angelozerr <angelo.zerr@gmail.com>
2017-11-14 12:34:10 +0100

    clean codelens monitor.

--- org.eclipse.jface.text.codelens/src/org/eclipse/jface/text/codelens/CodeLensMonitor.java
+++ org.eclipse.jface.text.codelens/src/org/eclipse/jface/text/codelens/CodeLensMonitor.java
@@ -1,14 +1,33 @@
+/**
+ *  Copyright (c) 2017 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - Provide CodeLens support - Bug XXXXXX
+ */
 package org.eclipse.jface.text.codelens;
 
+import java.util.concurrent.CancellationException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
+/**
+ * Monitor which throws a {@link CancellationException} when
+ * {@link IProgressMonitor#isCanceled()} is called.
+ *
+ */
 class CodeLensMonitor extends NullProgressMonitor {
 
 	@Override
 	public boolean isCanceled() {
-		if (super.isCanceled()) {
-			System.err.println("cancel!!");
+		boolean canceled = super.isCanceled();
+		if (canceled) {
+			throw new CancellationException();
 		}
-		return super.isCanceled();
+		return canceled;
 	}
 }
