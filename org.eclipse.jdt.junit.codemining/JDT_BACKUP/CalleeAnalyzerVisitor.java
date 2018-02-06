160ac801aecb25d627356a5876bcd5cbae55e785
angelozerr <angelo.zerr@gmail.com>
2018-02-06 19:12:46 +0100

    Backup

---
+++ org.eclipse.jdt.junit.codemining/JDT_BACKUP/CalleeAnalyzerVisitor.java
@@ -0,0 +1,46 @@
+package org.eclipse.jdt.internal.ui.javaeditor.codemining;
+
+import java.util.List;
+
+import org.eclipse.jface.text.codemining.ICodeMining;
+import org.eclipse.jface.text.codemining.ICodeMiningProvider;
+
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
+import org.eclipse.jdt.internal.corext.dom.HierarchicalASTVisitor;
+
+public class CalleeAnalyzerVisitor extends HierarchicalASTVisitor {
+
+	private final CompilationUnit cu;
+
+	private final List<ICodeMining> minings;
+
+	private final ICodeMiningProvider provider;
+
+	public CalleeAnalyzerVisitor(CompilationUnit cu, List<ICodeMining> minings, ICodeMiningProvider provider) {
+		this.cu= cu;
+		this.minings= minings;
+		this.provider= provider;
+	}
+
+	public boolean visit(MethodInvocation node) {
+		List arguments= node.arguments();
+		if (arguments.size() > 0) {
+
+			for (int i= 0; i < arguments.size(); i++) {
+				Expression exp= (Expression)arguments.get(i);
+				minings.add(new JavaMethodParameterCodeMining(node, exp, i, cu, provider));
+			}
+		}
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(VariableDeclarationStatement node) {
+
+		return true;
+	}
+}
